3e06abae402c0cc5e1ff88ebedb39effa28c0d14
M. Afifudin <afif.x.udin@gmail.com>
2022-07-17 23:28:41 +0700

    perf(api): cache response

--- src/pages/api/pokemon.ts
+++ src/pages/api/pokemon.ts
@@ -3,12 +3,22 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import { API_ENDPOINT } from '@/api/fetcher';
 
+const cache = new Map();
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { query } = req;
   try {
     const document = query.q as string;
     const variables = JSON.parse(query.v as string);
+
+    const cachedResponse = cache.get(`${document}#${query.v}`);
+    if (cachedResponse) {
+      res.status(200).json(cachedResponse);
+      return;
+    }
+
     const data = await request(API_ENDPOINT, document, variables);
+    cache.set(`${document}#${query.v}`, data);
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json(JSON.parse(JSON.stringify(error, Object.getOwnPropertyNames(error))));

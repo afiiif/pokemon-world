3779b77827d00598ee206aee18c0ae3fd70c5189
M. Afifudin <afif.x.udin@gmail.com>
2022-08-24 08:14:02 +0700

    test: fix webServer command

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ const config: PlaywrightTestConfig = {
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: 'npm run dev',
+    command: 'yarn prebuild && yarn dev',
     url: baseURL,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,

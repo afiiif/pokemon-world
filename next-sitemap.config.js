d30802acd5ce8f269cef26436d833b4f772f64d1
M. Afifudin <afif.x.udin@gmail.com>
2022-09-01 12:29:40 +0700

    fix(sitemap): fix pokemon with variant URL & adjust priority

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,10 +5,16 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_BASE_URL,
   generateRobotsTxt: true,
   additionalPaths: () =>
-    pokemons.map((pokemonName) => ({
-      loc: `/pokemon/${pokemonName}`,
-      changefreq: 'monthly',
-      priority: 0.7,
-      lastmod: new Date().toISOString(),
-    })),
+    pokemons.map((pokemonName) => {
+      const [mainName, variant = ''] = pokemonName.split('-');
+      const hasVariant = variant.length > 1;
+      const href = hasVariant ? `${mainName}/${pokemonName}` : pokemonName;
+
+      return {
+        loc: `/pokemon/${href}`,
+        changefreq: 'monthly',
+        priority: 0.6,
+        lastmod: new Date().toISOString(),
+      };
+    }),
 };

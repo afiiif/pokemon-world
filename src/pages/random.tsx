3cfc5850defd1af4b3456bafa20de15c8cc9f9dc
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-11-13 20:02:20 +0100

    Improvments to the new page

--- src/pages/random.tsx
+++ src/pages/random.tsx
@@ -1,32 +1,61 @@
+import { NextSeo } from 'next-seo';
 import { useEffect, useState } from 'react';
+import { HiOutlineRefresh } from 'react-icons/hi';
 
 import { Pokemon, randomPokemon } from '@/api/queries/random-pokemon';
 import PokemonCard from '@/features/pokemon-list/components/pokemon-card';
+import PokemonCardsShimmer from '@/features/pokemon-list/components/pokemon-cards-shimmer';
 
 export default function RandomPage() {
-  const POKEMON_COUNT = 9;
+  const POKEMON_COUNT = 12;
   const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
+  const randomize = () => {
     const tempNames: Promise<Pokemon>[] = [];
+    setIsLoading(true);
+    setPokemon([]);
 
     for (let i = 0; i < POKEMON_COUNT; i += 1) tempNames.push(randomPokemon());
 
-    Promise.all(tempNames).then((res) => setPokemon(res));
-  }, []);
+    Promise.all(tempNames).then((res) => {
+      setPokemon(res);
+      setIsLoading(false);
+    });
+  };
+
+  useEffect(() => randomize, []);
 
   return (
-    <div>
-      {pokemon.map((p) => (
-        <div key={p.id}>
-          <PokemonCard
-            id={p.id}
-            name={p.name}
-            key={p.id}
-            pokemon_v2_pokemons={p.pokemon_v2_pokemons}
-          />
-        </div>
-      ))}
-    </div>
+    <>
+      <NextSeo title="Random Pokemon" />
+
+      <div className="flex items-baseline justify-between">
+        <h1 className="h1 pb-6">Random Pokemon</h1>
+
+        <button
+          type="button"
+          onClick={randomize}
+          className="flex items-center gap-2 rounded-md bg-blue-500 px-3 py-2 text-white dark:bg-blue-800"
+        >
+          <HiOutlineRefresh className="text-2xl" />
+          Randomize
+        </button>
+      </div>
+
+      <div className="pokemon-card-container">
+        {pokemon.map((p) => (
+          <div key={p.id}>
+            <PokemonCard
+              id={p.id}
+              name={p.name}
+              key={p.id}
+              pokemon_v2_pokemons={p.pokemon_v2_pokemons}
+            />
+          </div>
+        ))}
+        {isLoading && <PokemonCardsShimmer />}
+      </div>
+    </>
   );
 }
